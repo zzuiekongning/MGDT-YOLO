22ecd48338e2c80243bad288789c2f0709c70da9
kong ning <1534205035@qq.com>
2025-08-24 22:21:17 +0800

    Update pnnx_conver.py

--- nn/pnnx_conver.py
+++ nn/pnnx_conver.py
@@ -2,7 +2,7 @@ import torch
 from ultralytics import YOLO
 import pnnx
 
-# 加载 PyTorch 训练好的模型
+# Load trained model
 x= torch.rand(1,3,640,640)
 model = YOLO("/media/robot/7846E2E046E29DDE/ultralytics/runs/detect/using_mspa_c2f_gd/train/weights/best.pt")
 
