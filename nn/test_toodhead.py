79d4b943d6562b97fff40051e46f4c60843607b3
kong ning <1534205035@qq.com>
2025-08-24 22:27:46 +0800

    Update

--- nn/test_toodhead.py
+++ nn/test_toodhead.py
@@ -2,41 +2,40 @@ import torch
 from ultralytics.nn.modules import TOODHead
 import torch.nn as nn
 
-# # 假设输入张量
+# Example: Input tensor
 # x = torch.randn(4, 128, 80, 80)  # [batch_size, channels, height, width]
 
-# # 定义 GroupNorm
-# num_groups = 16  # 确保 128 % 16 == 0
+# # Define GroupNorm
+# num_groups = 16  # Ensure 128 % 16 == 0
 # norm_layer = nn.GroupNorm(num_groups=num_groups, num_channels=128)
 
-# # 应用归一化
+# # Apply normalization
 # output = norm_layer(x)
-# print(output.shape)  # 应保持 [4, 128, 80, 80]
+# print(output.shape)  # Should remain [4, 128, 80, 80]
 
 
-# 示例：3层特征图，每层维度为 (batch_size, channels, height, width)
+# Example: 3-layer feature map, each with shape (batch_size, channels, height, width)
 batch_size = 1
-c =  256
+c = 256
 h = 80
 w = 80
 # feature_shapes = [256, 80, 80]
 features = torch.rand(batch_size, c, h, w)
 
+# Initialize the detection head
+nc = 2  # Number of classes
+hidc = 256  # Hidden layer channels
+# block_num = 2  # Number of blocks
+ch = [128]  # Input channels
 
+detect_head = TOODHead(nc=nc, hidc=hidc, ch=ch)
 
-# 初始化检测头
-nc = 2  # 类别数量
-hidc = 256  # 隐藏层通道数
-# block_num = 2  # 块的数量
-ch = [128] # 输入通道数
-
-detect_head = TOODHead(nc= nc, hidc = hidc, ch=ch)
-
-# 前向传播
+# Forward pass
 output = detect_head.forward(features)
 
-# 打印输出结果
+# Print the output
 print("Output:", output)
+
 
 
 
