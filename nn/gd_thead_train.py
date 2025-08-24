1c1bdba22019dc6bfa854938652a874e4578239d
kong ning <1534205035@qq.com>
2025-08-24 22:14:59 +0800

    Comments to English

--- nn/gd_thead_train.py
+++ nn/gd_thead_train.py
@@ -9,9 +9,9 @@ print("************************")
 
 model.load('yolov8n.pt')
 
-# # 训练模型
-data_path = '/media/robot/7846E2E046E29DDE/piglet_pic_from_net/data.yaml'  # 替换为数据集配置文件路径
+# # Train the model
+data_path = '/media/robot/7846E2E046E29DDE/piglet_pic_from_net/data.yaml'  # Replace with dataset config file path
 model.train(
-    data=data_path,          # 数据集配置路径
-    cfg = '/media/robot/7846E2E046E29DDE/paper_code_source/our_ultralytics/ultralytics/yolo/cfg/default.yaml'
+    data=data_path,          # Dataset configuration path
+    cfg='/media/robot/7846E2E046E29DDE/paper_code_source/our_ultralytics/ultralytics/yolo/cfg/default.yaml'
 )
